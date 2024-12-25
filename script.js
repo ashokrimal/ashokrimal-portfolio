104f47505c7677f2c225d5f02f9cca091a78c2f0
Ashok Rimal <122424817+ashokrimal@users.noreply.github.com>
2024-12-25 17:34:13 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const mobileNav = document.querySelector(".hamburger");
+const navbar = document.querySelector(".menubar");
+
+const toggleNav = () => {
+  navbar.classList.toggle("active");
+  mobileNav.classList.toggle("hamburger-active");
+};
+mobileNav.addEventListener("click", () => toggleNav());
+
+
+
+
+function submitted(){
+  alert("Your message has been sent!!");
+}
