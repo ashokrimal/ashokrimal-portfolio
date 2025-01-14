0426a7fe833aa86bf1166048c6f224f1c1cd1c42
Ashok Rimal <122424817+ashokrimal@users.noreply.github.com>
2025-01-14 17:28:40 +0545

    Add files via upload

--- script.js
+++ script.js
@@ -8,8 +8,31 @@ const toggleNav = () => {
 mobileNav.addEventListener("click", () => toggleNav());
 
 
+// Select the toggle button
+const darkModeToggle = document.getElementById('darkModeToggle');
 
+// Check for saved user preference in localStorage
+const currentMode = localStorage.getItem('darkMode');
+
+// Apply the saved mode
+if (currentMode === 'enabled') {
+  document.body.classList.add('dark-mode');
+}
+
+// Add event listener to the toggle button
+darkModeToggle.addEventListener('click', () => {
+  const isDarkMode = document.body.classList.toggle('dark-mode');
+
+  // Save the user's preference
+  if (isDarkMode) {
+    localStorage.setItem('darkMode', 'enabled');
+  } else {
+    localStorage.setItem('darkMode', 'disabled');
+  }
+});
 
 function submitted(){
   alert("Your message has been sent!!");
+  document.getElementById("form-group").reset();
 }
+
